cfeb82b2cd971da8e1233036f4c6095df523ddae
unknown <USRSIS0129@ESTSIS0129.cobeca.com.ve>
2016-04-26 15:02:26 -0430

    Updated progress bar, timer

--- SanaCompetencia.Server/app/controller.js
+++ SanaCompetencia.Server/app/controller.js
@@ -1,70 +1,35 @@
 ﻿'use strict'
 
 app.controller('DataController', ['$rootScope', '$scope', 'backendHubProxy', function ($rootScope, $scope, backendHubProxy) {
-  $rootScope.pollInterval = 5000 // Tiempo para refrescar la información
-  $scope.cycleInterval = 120000 // Tiempo para cambiar diario o mensual
-  $rootScope.monthly = false // Para identificar si la información sera diaria o mensual
   var dataHub = backendHubProxy(backendHubProxy.defaultServer, 'dataHub')
-
-  $scope.title = "ACUMULADO DIARIO"
-  var bar = $('#time-left')
   $rootScope.setTitle = function (title) {
     $scope.title = title
   }
-
-  $scope.progressBarRestart = function () {
-    clearInterval($scope.progressBarInterval)
-    bar.attr('aria-valuenow', ($scope.cycleInterval / 1000))
-    bar.animate({ width: '100%' })
-    $scope.progressBarStart()
-  }
-
-  $scope.progressBarStart = function () {
-    $scope.progressBarInterval = setInterval(function () {
-      var bar_value = bar.attr('aria-valuenow')
-      var width = (bar_value * 100) / ($scope.cycleInterval / 1000)
-      var new_value = (bar_value - 1)
-      var width_value = width + '%'
-      bar.attr('aria-valuenow', new_value)
-      bar.animate({ width: width_value }, {
-        duration: 1000,
-        easing: 'linear'
-      })
-    }, 1000)
-  }
+  $scope.title = "ACUMULADO DIARIO"
 
   // Cada cuanto tiempo se cambia entre diario y mensual
-  $scope.progressBarStart()
-  var cycleInterval = setInterval(function () {
-    $scope.progressBarRestart()
-    $rootScope.monthly = !$rootScope.monthly
-    $scope.monthly = $rootScope.monthly
-  }, $scope.cycleInterval)
 
   // Objeto inicial para cada indicador
-  $scope.recepcion = [
-    { drogueria: 3, valor: 0, nombre: 'MAFARTA' },
-    { drogueria: 4, valor: 0, nombre: 'DROMERCA' },
-    { drogueria: 7, valor: 0, nombre: 'OCCIDENTE' },
-    { drogueria: 13, valor: 0, nombre: 'COBECA CENTRO' },
-    { drogueria: 14, valor: 0, nombre: 'COBECA BQTO' },
-    { drogueria: 16, valor: 0, nombre: 'ORIENTE' }
-  ]
-  $scope.almacen = $scope.recepcion
-  $scope.cesta = $scope.recepcion
-  $scope.bulto = $scope.recepcion
-  $scope.trafico = $scope.recepcion
-  $scope.devolucion = $scope.recepcion
+  $scope.recepcionArray = initArray([])
+  $scope.almacenArray = initArray([])
+  $scope.cestaArray = initArray([])
+  $scope.bultoArray = initArray([])
+  $scope.devolucionArray = initArray([])
+  $scope.traficoArray = initArray([])
 
+  function initArray(array) {
+    var result = []
+    var length = 6 - array.length
+      for (var i = 0; i < length; i++) {
+        result.push(i)
+      }
+      return result
+  }
   // Callback de SignalR por cada indicador, si hay información se borra el arreglo actual y se actualiza
   dataHub.on('broadcastRecepcion', function (data) {
     if (data.length > 0 && data.length <= 6) {
       $scope.recepcion = [];
-      $scope.recepcionLength = 6 - data.length
-      $scope.recepcionArray = []
-      for (var i = 0; i < $scope.recepcionLength; i++) {
-        $scope.recepcionArray.push(i)
-      }
+      $scope.recepcionArray = initArray(data)
     }
     data.forEach(function (recepcion) {
       $scope.recepcion.push(recepcion)
@@ -74,11 +39,7 @@ app.controller('DataController', ['$rootScope', '$scope', 'backendHubProxy', fun
   dataHub.on('broadcastAlmacen', function (data) {
     if (data.length > 0 && data.length <= 6) {
       $scope.almacen = []
-      $scope.almacenLength = 6 - data.length
-      $scope.almacenArray = []
-      for (var i = 0; i < $scope.almacenLength; i++) {
-        $scope.almacenArray.push(i)
-      }
+      $scope.almacenArray = initArray(data)
     }
     data.forEach(function (almacen) {
       $scope.almacen.push(almacen)
@@ -88,11 +49,7 @@ app.controller('DataController', ['$rootScope', '$scope', 'backendHubProxy', fun
   dataHub.on('broadcastCestas', function (data) {
     if (data.length > 0 && data.length <= 6) {
       $scope.cesta = []
-      $scope.cestaLength = 6 - data.length
-      $scope.cestaArray = []
-      for (var i = 0; i < $scope.cestaLength; i++) {
-        $scope.cestaArray.push(i)
-      }
+      $scope.cestaArray = initArray(data)
     }
     data.forEach(function (cesta) {
       $scope.cesta.push(cesta)
@@ -102,11 +59,7 @@ app.controller('DataController', ['$rootScope', '$scope', 'backendHubProxy', fun
   dataHub.on('broadcastBulto', function (data) {
     if (data.length > 0 && data.length <= 6) {
       $scope.bulto = []
-      $scope.bultoLength = 6 - data.length
-      $scope.bultoArray = []
-      for (var i = 0; i < $scope.bultoLength; i++) {
-        $scope.bultoArray.push(i)
-      }
+      $scope.bultoArray = initArray(data)
     }
     data.forEach(function (bulto) {
       $scope.bulto.push(bulto)
@@ -116,11 +69,7 @@ app.controller('DataController', ['$rootScope', '$scope', 'backendHubProxy', fun
   dataHub.on('broadcastDevolucion', function (data) {
     if (data.length > 0 && data.length <= 6) {
       $scope.devolucion = [];
-      $scope.devolucionLength = 6 - data.length
-      $scope.devolucionArray = []
-      for (var i = 0; i < $scope.devolucionLength; i++) {
-        $scope.devolucionArray.push(i)
-      }
+      $scope.devolucionArray = initArray(data)
     }
     data.forEach(function (devolucion) {
       $scope.devolucion.push(devolucion)
@@ -130,11 +79,7 @@ app.controller('DataController', ['$rootScope', '$scope', 'backendHubProxy', fun
   dataHub.on('broadcastTrafico', function (data) {
     if (data.length > 0 && data.length <= 6) {
       $scope.trafico = [];
-      $scope.traficoLength = 6 - data.length
-      $scope.traficoArray = []
-      for (var i = 0; i < $scope.traficoLength; i++) {
-        $scope.traficoArray.push(i)
-      }
+      $scope.traficoArray = initArray(data)
     }
     data.forEach(function (trafico) {
       $scope.trafico.push(trafico)
--- SanaCompetencia.Server/app/services.js
+++ SanaCompetencia.Server/app/services.js
@@ -1,16 +1,24 @@
 ﻿'use strict'
 
 app.factory('backendHubProxy', ['$rootScope', 'backendServerUrl', function ($rootScope, backendServerUrl) {
+  $rootScope.pollInterval = 5000 // Tiempo para refrescar la información
+  $rootScope.cycleInterval = 120000 // Tiempo para cambiar diario o mensual
+  $rootScope.monthly = false // Para identificar si la información sera diaria o mensual
+
   // Subscripcion a SignalR
   function backendFactory(serverUrl, hubName) {
     var connection = $.hubConnection(backendServerUrl)
     var proxy = connection.createHubProxy(hubName)
+    var bar = $('#time-left')
 
     // Callback cuando se conecta al Hub
     connection.start().done(function () {
       proxy.invoke('SendData')
+      progressBarStart()
       // Cada cuanto tiempo se refresca la información
       setInterval(function () {
+        progressBarRestart()
+        $rootScope.monthly = !$rootScope.monthly
         if ($rootScope.monthly) {
           $rootScope.setTitle("ACUMULADO MENSUAL")
           proxy.invoke('SendDataMonthly')
@@ -18,8 +26,29 @@ app.factory('backendHubProxy', ['$rootScope', 'backendServerUrl', function ($roo
           $rootScope.setTitle("ACUMULADO DIARIO")
           proxy.invoke('SendData')
         }
-      }, $rootScope.pollInterval)
+      }, $rootScope.cycleInterval - 1000)
     })
+
+    function progressBarStart() {
+      $rootScope.progressBarInterval = setInterval(function () {
+        var bar_value = bar.attr('aria-valuenow')
+        var width = (bar_value * 100) / ($rootScope.cycleInterval / 1000)
+        var new_value = (bar_value - 1)
+        var width_value = width + '%'
+        bar.attr('aria-valuenow', new_value)
+        bar.animate({ width: width_value }, {
+          duration: 1000,
+          easing: 'linear'
+        })
+      }, 1000)
+    }
+
+    function progressBarRestart() {
+      clearInterval($rootScope.progressBarInterval)
+      bar.attr('aria-valuenow', ($rootScope.cycleInterval / 1000))
+      bar.animate({ width: '100%' })
+      progressBarStart()
+    }
 
     // Métodos a utilizar (on, invoke)
     return {
